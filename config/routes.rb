1c299d9dde4327320b6c3d99bbee9e8a1e2c735d
2011140071b <2011140071b@ed.fuk.kindai.ac.jp>
2022-10-19 07:09:41 +0000

    controller, model を設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'carts/show'
+  get 'cart_items/new'
+  get 'cart_items/create'
+  get 'cart_items/destroy'
+  get 'top/main'
+  get 'products/index'
+  get 'products/new'
+  get 'products/create'
+  get 'products/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
